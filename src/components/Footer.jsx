852a2cbcb45c045b9a35370bab990526d76d7792
Armando Valencia <armando96valencia@gmail.com>
2023-02-28 00:45:18 -0600

    Install CharkaUI, work on experience, tweak css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import { GrGithub, GrLinkedin } from "react-icons/gr";
 
 const Footer = () => {
+	const githubUrl = "https://github.com/thearmandovalencia/portfolio";
+	const linkedinUrl = "https://www.linkedin.com/in/armandovalencia1/";
+
 	return (
-		<div className="flex justify-center p-6 gap-2">
-			<GrGithub /> <GrLinkedin />
+		<div className="flex justify-center p-6 mt-8 gap-2 border-t border-secondary">
+			<a href={githubUrl} target="_blank">
+				<GrGithub />
+			</a>
+			<a href={linkedinUrl} target="_blank">
+				<GrLinkedin />
+			</a>
 		</div>
 	);
 };
