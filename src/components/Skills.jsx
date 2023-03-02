8ef6bc9f8b6ea50aa58630b058a61dc27384e565
Armando Valencia <armando96valencia@gmail.com>
2023-03-02 15:13:31 -0600

    Create skeleton for home page

---
+++ src/components/Skills.jsx
@@ -0,0 +1,17 @@
+const Skills = () => {
+	return (
+		<div className="grid grid-cols-3 gap-1 p-2 justify-items-center list-none">
+			<li>React</li>
+			<li>JavaScript</li>
+			<li>Node</li>
+			<li>Python</li>
+			<li>Rust</li>
+			<li>Tailwind</li>
+			<li>Git</li>
+			<li>MongoDB</li>
+			<li>SQL</li>
+		</div>
+	);
+};
+
+export default Skills;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Experience from "../components/Experience";
 import About from "../components/About";
+import Skills from "../components/Skills";
 
 const Home = () => (
 	<div className="px-6 pt-6 md:grid grid-cols-3">
@@ -7,10 +8,22 @@ const Home = () => (
 		<div>
 			<div id="home" className=""></div>
 			<div id="about" className="">
+				<div className="flex justify-center m-3">
+					<h1 className="text-3xl">01. About</h1>
+				</div>
 				<About />
 			</div>
 			<div id="experience" className="">
+				<div className="flex justify-center m-3">
+					<h1 className="text-3xl">02. Experience</h1>
+				</div>
 				<Experience />
+			</div>
+			<div id="skills" className="pt-4">
+				<div className="flex justify-center m-3">
+					<h1 className="text-3xl">03. Skills</h1>
+				</div>
+				<Skills />
 			</div>
 		</div>
 		<div></div>
