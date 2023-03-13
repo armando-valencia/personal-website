be08379327286b87e896c9654fff964fc2a688d4
Armando Valencia <armando96valencia@gmail.com>
2023-03-13 03:06:49 -0500

    Home page animation and increase font size

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 
 function App() {
 	return (
-		<div className="text-quarternary font-nunito bg-black/70">
+		<div className="text-quarternary font-nunito bg-black/80">
 			<Nav />
 			<HomePage />
 			<Footer />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,38 +7,62 @@ const containerVariants = {
 	visible: { opacity: 1, transition: { delay: 1, duration: 1.5 } },
 };
 
+const imgVariants = {
+	hidden: {
+		y: -2000,
+	},
+	visible: {
+		y: 0,
+		transition: {
+			yoyo: 10,
+			delay: 0.3,
+			type: "tween",
+			duration: 3,
+		},
+	},
+};
+
+const menuVariants = {
+	hidden: { opacity: 0 },
+	visible: { opacity: 1, transition: { delay: 10.7, duration: 0.3 } },
+};
+
 const Home = () => {
 	//TODO: when hovering over the menu options, add an arrow to the left of the text (maybe using framer motion?)
 	const menuItems = ["About", "Experience", "Contact"];
 	return (
 		<motion.div
 			className="text-tertiary p-2 text-2xl md:text-4xl flex items-center flex-col"
-			variants={containerVariants}
+			// variants={containerVariants}
 			initial="hidden"
 			animate="visible"
 		>
 			<div className="flex justify-center">
-				<img
-					src="\picture.jpg"
-					alt="Picture of myself"
-					className="rounded h-44 px-3 pb-3 mb-4"
+				<motion.img
+					variants={imgVariants}
+					src="\ARMANDO.png"
+					alt=""
+					className="rounded h-44 md:h-60 px-3 pb-3 mb-4"
 				/>
 			</div>
 			<TypeAnimation
 				sequence={[
 					"",
-					2000,
+					4000,
 					"Hey!",
 					1000,
 					"Hey! My name is Armando Valencia.",
-					1300,
+					1200,
 					"Hey! My name is Armando Valencia. I like to build things.",
 					1300,
 				]}
 				speed={40}
 			/>
 
-			<div className="list-none text-quarternary pt-4">
+			<motion.div
+				variants={menuVariants}
+				className="list-none text-quarternary pt-4"
+			>
 				{menuItems.map((menuItem, idx) => (
 					<li className="text-left md:text-3xl p-2 md:p-3" key={idx}>
 						<a
@@ -49,7 +73,7 @@ const Home = () => {
 						</a>
 					</li>
 				))}
-			</div>
+			</motion.div>
 		</motion.div>
 	);
 };
